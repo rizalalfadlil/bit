7dc784bb868954f380db677150290b31b574ce78
RizalAlfadlil <rizalalfadlil@gmail.com>
2024-01-20 10:08:52 +0700

    b

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { Input, Progress, Result, Spin, Image } from 'antd';
+import { Input, Progress, Image, Tooltip } from 'antd';
 import './App.css';
-import { Container } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {
   LoadingOutlined,
+  InfoCircleOutlined
 } from '@ant-design/icons';
 
 const App = () => {
@@ -12,11 +12,8 @@ const App = () => {
   const [hasil, setHasil] = useState(null);
   const [color, setColor] = useState(null);
   const [loading, setLoading] = useState(false);
-  const TeksHasil = () =>{
-    if(color === 'success')return 'good';
-    else if (color === 'warning')return 'ok';
-    else return 'bad';
-  }
+  const [teksHasil, setTeksHasil] = useState('teks');
+  const [image, setImage] = useState();
 
   const twoColors = {
     '100%': '#FF8076',
@@ -44,15 +41,28 @@ const App = () => {
       
       const persentaseBaik = hash(nama);
       setHasil(persentaseBaik);
-      if(persentaseBaik > 200/3){
-        setColor('success')
-      }else if(persentaseBaik <200/3 && persentaseBaik >100/3){
-        setColor('warning')
-      }else{
+      if(persentaseBaik > 80){
+        setTeksHasil('bit karbittt... Raja karbit Dewa karbit bit... bit...')
+        setColor('dark');
+        setImage('5.jpeg');
+      }else if(persentaseBaik <80 && persentaseBaik >60){
+        setTeksHasil('ini dia Raja karbit');
         setColor('danger');
+        setImage('4.jpeg')
+      }else if(persentaseBaik <60 && persentaseBaik >40){
+        setTeksHasil('Lu karbit');
+        setColor('danger');
+        setImage('3.jpeg');
+      }else if(persentaseBaik <40 && persentaseBaik >20){
+        setTeksHasil('Kok kamu karbit bang?')
+        setColor('warning');
+        setImage('2.jpeg')
+      }else {
+        setTeksHasil('Kamu setia');
+        setColor('success');
+        setImage('1.gif');
       }
       
-      // Tambahkan delay sebelum loading dihilangkan (contoh: 2 detik)
       setLoading(false);
     };
 
@@ -65,8 +75,8 @@ const App = () => {
     <div className="App bg-secondary bg-opacity-25">
       <div className='p-2 d-flex align-items-center justify-content-center' style={{height:'100vh'}}>
       <div className='border p-5 container bg-light rounded'>
-      <h1 className='fw-bold'>Title</h1>
-      <h3 className='mb-5 text-secondary fw-light'>subtitle</h3>
+      <h1 className='fw-bold'>Seberapa karbit kamu</h1>
+      <h3 className='mb-5 text-secondary fw-light'>Tulis namamu</h3>
       <Input
         placeholder="Tulis Nama"
         value={nama}
@@ -78,15 +88,17 @@ const App = () => {
       {loading ? (
         <LoadingOutlined style={{scale:'2', color:'#4A95FF'}}/>
       ) : (
-        hasil !== null && (
+        (hasil !== null && nama !== '') && (
           <>
           <Progress percent={hasil} className="rounded-0" size={['default', 30]} format={(percent) => percent} strokeColor={twoColors}/>
           <div className='row fw-bold mt-2 '>
-            <span className='col text-start text-success'>good</span>
-            <span className='col text-end text-danger'>bad</span>
+            <span className='col text-start text-success'>setia</span>
+            <span className='col text-end text-danger'>karbit</span>
           </div>
-          <h1 className={`text-${color}`}><TeksHasil/></h1>
-          <Image src='logo192.png' preview={false}/>
+          <h5>{nama} {hasil}%, Kamu...</h5>
+          <h1 className={`text-${color} fw-bold`}>{teksHasil}</h1>
+          <Image src={image} preview={false} width={200}/>
+          <p className='text-end w-100'><Tooltip title='hanya untuk candaan dan bukan beneran'><InfoCircleOutlined/></Tooltip></p>
           </>
         )
       )}
