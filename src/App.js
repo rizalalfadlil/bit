eeb27f23e1224e63a8dfb25d15715ca95cbe775c
RizalAlfadlil <rizalalfadlil@gmail.com>
2024-01-20 18:23:58 +0700

    h

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Input, Progress, Image, Tooltip } from 'antd';
+import { Input, Progress, Image, Tooltip, Button } from 'antd';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {
@@ -9,6 +9,7 @@ import {
 
 const App = () => {
   const [nama, setNama] = useState('');
+  const [teksNama, setTeksNama] = useState(nama);
   const [hasil, setHasil] = useState(null);
   const [color, setColor] = useState(null);
   const [loading, setLoading] = useState(false);
@@ -20,13 +21,10 @@ const App = () => {
     '50%': '#FFEA76',
     '0%': '#4AFF91',
   };
-
-  useEffect(() => {
     const hitungKesejahteraan = () => {
       setLoading(true);
-
+      setTeksNama(nama);
       
-
       // Menggunakan fungsi hash sederhana untuk menghasilkan nilai acak yang konsisten berdasarkan nama
       const hash = (str) => {
         let hashValue = 0;
@@ -62,21 +60,18 @@ const App = () => {
         setColor('success');
         setImage('1.gif');
       }
-      
-      setLoading(false);
+      setTimeout(() => {
+        setLoading(false);
+      }, 1000);
     };
-
-    if (nama) {
-      hitungKesejahteraan();
-    }
-  }, [nama]);
 
   return (
     <div className="App bg-secondary bg-opacity-25">
       <div className='p-2 d-flex align-items-center justify-content-center' style={{height:'100vh'}}>
       <div className='border p-5 container bg-light rounded'>
       <h1 className='fw-bold'>Seberapa karbit kamu</h1>
-      <h3 className='mb-5 text-secondary fw-light'>Tulis namamu</h3>
+      <h3 className='text-secondary fw-light'>Tulis namamu</h3>
+      <div className='row'>
       <Input
         placeholder="Tulis Nama"
         value={nama}
@@ -84,20 +79,22 @@ const App = () => {
         onChange={(e) => setNama(e.target.value)}
         style={{ marginBottom: '16px' }}
       />
+      <Button type='primary' disabled={nama == ''} className='rounded-pill' onClick={hitungKesejahteraan}>Hitung</Button>
+      </div>
       <div className='mt-3'>
       {loading ? (
         <LoadingOutlined style={{scale:'2', color:'#4A95FF'}}/>
       ) : (
-        (hasil !== null && nama !== '') && (
+        (hasil !== null) && (
           <>
           <Progress percent={hasil} className="rounded-0" size={['default', 30]} format={(percent) => percent} strokeColor={twoColors}/>
           <div className='row fw-bold mt-2 '>
             <span className='col text-start text-success'>setia</span>
             <span className='col text-end text-danger'>karbit</span>
           </div>
-          <h5>{nama} {hasil}%, Kamu...</h5>
+          <h5 className='text-uppercase'>{teksNama} {hasil}%, Kamu...</h5>
           <h1 className={`text-${color} fw-bold`}>{teksHasil}</h1>
-          <Image src={image} preview={false} width={200}/>
+          <Image src={image} preview={false} width={200} style={{maxHeight:'250px'}}/>
           <p className='text-end w-100'><Tooltip title='hanya untuk candaan dan bukan beneran'><InfoCircleOutlined/></Tooltip></p>
           </>
         )
