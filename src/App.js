5b81409e7af57309e2bdea613f69219c2dd377c8
Minin <70429604+MininxD@users.noreply.github.com>
2024-01-20 19:22:21 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -39,19 +39,19 @@ const App = () => {
       
       const persentaseBaik = hash(nama);
       setHasil(persentaseBaik);
-      if(persentaseBaik > 80){
+      if(persentaseBaik >= 80){
         setTeksHasil('bit karbittt... Raja karbit Dewa karbit bit... bit...')
         setColor('dark');
         setImage('5.jpeg');
-      }else if(persentaseBaik <80 && persentaseBaik >60){
+      }else if(persentaseBaik >= 60 && persentaseBaik >60){
         setTeksHasil('ini dia Raja karbit');
         setColor('danger');
         setImage('4.jpeg')
-      }else if(persentaseBaik <60 && persentaseBaik >40){
+      }else if(persentaseBaik >= 40 && persentaseBaik >40){
         setTeksHasil('Lu karbit');
         setColor('danger');
         setImage('3.jpeg');
-      }else if(persentaseBaik <40 && persentaseBaik >20){
+      }else if(persentaseBaik >= 35 && persentaseBaik >20){
         setTeksHasil('Kok kamu karbit bang?')
         setColor('warning');
         setImage('2.jpeg')
